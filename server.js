97df0170ee2306cb1209d98df6eb95816213278d
Suzan-Hiary <suze-hiary@hotmail.com>
2021-08-17 02:44:20 +0300

    lab12

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/',(req,res)=>{
   res.send('Loading')
 });
 
+const client = jwksClient({
+  jwksUri: `https://${process.env.REACT_APP_AUTH0_DOAMIN}/.well-known/jwks.json`
+});
+
 const getKey=(header, callback)=>{
   client.getSigningKey(header.kid, function(err, key) {
       const signingKey = key.publicKey || key.rsaPublicKey;
@@ -24,13 +28,13 @@ app.get('/test', (request, response) => {
 
   // TODO: 
   // STEP 1: get the jwt from the headers
-  const jwtHed =req.headers.authorization.split(' ')[1];
+  const jwtHed =request.headers.authorization.split(' ')[1];
   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
   jwt.verify(jwtHed ,getKey,{},(err,user)=>{
     if(err){
-        res.json('invalid token');
+        response.json('invalid token');
     }
-    res.json(user)
+    response.json(user)
 })
   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
