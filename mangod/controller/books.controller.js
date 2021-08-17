0b46b3a42ab0fcc1b58af98db6bb171cfe7dbf46
Suzan-Hiary <suze-hiary@hotmail.com>
2021-08-17 23:45:11 +0300

    added

--- mangod/controller/books.controller.js
+++ mangod/controller/books.controller.js
@@ -1,13 +1,7 @@
 const Books =require("../models/books.models");
 
-const catController=(req,res)=>{
-    const newBooks = new Books ({
-        title:"Ulysses",
-        description:"Ulysses chronicles the passage of Leopold Bloom through Dublin during an ordinary day, June 16, 1904. The title parallels and alludes to Odysseus (Latinised into Ulysses), the hero of Homer's Odyss..",
-        status:"James Joyce" ,
-        email : "suzanhiary4@gmail.com"
-    })
-    newBooks.save();
+const bookController=(req,res)=>{
+    seedBook();
     res.send(newBooks);
 }
 
